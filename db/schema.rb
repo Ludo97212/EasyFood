05c0967f850ea4b0697dedfa93efe56bcb72188b
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-06-27 16:14:37 -0300

    modif schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_004701) do
+ActiveRecord::Schema.define(version: 2022_06_14_190151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_06_14_004701) do
     t.bigint "food_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "comment"
     t.index ["food_id"], name: "index_ratings_on_food_id"
     t.index ["user_id"], name: "index_ratings_on_user_id"
   end
