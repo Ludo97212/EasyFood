527f328c90961db298bc10fd3d56e34ade89b1dc
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-06-29 13:27:37 -0300

    modif design show category

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,8 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :food
+
+  validates :score, presence: true
+  validates :score, inclusion: { in: 0.0..5.0 }
+  validates :comment, length: { in: 3..250 }
 end
