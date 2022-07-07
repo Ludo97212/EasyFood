3b515698ba5eaf957cd179191c6d4fb87b541d97
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-07-07 11:30:28 -0300

    modif comments on show

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,20 +8,6 @@ class FoodsController < ApplicationController
     # TEST BEST_FOODS
     # @best_foods = Food.joins(:ratings).order_by(average(score: DESC)).first(8)
     # @best_foods = Food.ratings.average(:score).order_by(:desc).first(8)
-    @modal_text = "Depuis qu't'es parti, j'me prends la tête, j'ai lu tous les livres d'Hermann Hesse
-      L'impression de mieux comprendre tes textes et tes yeux glacés comme ceux des immortels
-      Comme tous ceux qui choisissent de se taire, t'avais l'énergie, l'aura puissante (Puissante)
-      J'm'en rappellerai toujours, même quand la vie n'aura plus d'sens
-      Enterrer nos armes dans la terre et le sable, on est l'espèce humaine
-      La pierre et le sabre, la parfaite lumière, ceux qui ont péri le savent
-      C'est toi qui as fait le logo, on le porte sur le cœur, comme tous ceux qu'on a perdus, c'est la vie ma le-gueu
-      Et le vice de l'orgueil, j'en veux plus dès lors que tu recollais les miens
-      Le cœur sur la main, ton binôme inconsolable
-      Depuis, même quand l'ciel est bleu, les Tortues Ninja courent sous la pluie
-      Loin de la surface, tout là-haut, les bons se retrouveront paraît-il
-      On espère que tu surferas si Dieu le veut, sur une des quatre rivières du Paradis
-      Crois pas qu'on t'oublie
-      "
   end
 
   def index
@@ -38,7 +24,7 @@ class FoodsController < ApplicationController
     set_food
     # create a rating on this show
     @rating = Rating.new
-    @f_ratings = @food.ratings.order(updated_at: :desc)
+    @f_ratings = @food.ratings.order(updated_at: :desc).limit(10)
     # ------------AVG SCORE------------------------------- #
     # Round(1) == Garder 1 chifffre apres la virgule
     @avg_score = @f_ratings.average(:score).to_f.round(1)
